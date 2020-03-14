b3d3b50c7ccd7d7caffa1c5455c25ca1b9c765f2
nazrin <nazrinaghayeva@gmail.com>
2020-03-14 23:51:34 +0400

    --on the way

--- src/main/java/az/company/booking_project/Exceptions/BookingException.java
+++ src/main/java/az/company/booking_project/Exceptions/BookingException.java
@@ -1,5 +1,8 @@
 package az.company.booking_project.Exceptions;
 
 public class BookingException extends RuntimeException {
+    public BookingException(String message) {
+        super(message);
+    }
 
 }
---
+++ src/main/java/az/company/booking_project/Exceptions/UserException.java
@@ -0,0 +1,8 @@
+package az.company.booking_project.Exceptions;
+
+public class UserException extends RuntimeException {
+    public UserException(String message) {
+        super(message);
+    }
+
+}
